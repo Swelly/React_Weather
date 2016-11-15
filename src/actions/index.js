809609de1119ba5bbee7a17f55f0725e1aac8f3f
Swelly <w.magness17@gmail.com>
2016-11-15 12:45:46 -0500

    axios implemented. returning FETCH_WEATHER payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = 'db4611cdd5d1a1c135f6d4f3b2b832b9';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 // Variable to hold Action Type
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`
+	// request == promise
+	const request = axios.get(url);
 
 	return {
 		type: FETCH_WEATHER
+		payload: request
 	};
 }
