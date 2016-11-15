fc3738fec8cafc63aa9ab2801a5046455335db4c
Swelly <w.magness17@gmail.com>
2016-11-15 13:07:35 -0500

    fetching weather with ajax request

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,15 +4,14 @@ const API_KEY = 'db4611cdd5d1a1c135f6d4f3b2b832b9';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 // Variable to hold Action Type
-export const FETCH_WEATHER = 'FETCH_WEATHER'
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-	const url = `${ROOT_URL}&q=${city},us`
-	// request == promise
+	const url = `${ROOT_URL}&q=${city},us`;
 	const request = axios.get(url);
 
 	return {
-		type: FETCH_WEATHER
+		type: FETCH_WEATHER,
 		payload: request
 	};
 }
